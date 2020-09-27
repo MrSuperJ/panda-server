cf177741b1b611258f9a88d3acbce7efcf86d69d
ewall <1054064180@qq.com>
2020-09-27 22:31:17 +0800

    update .gitignore

--- config/index.js
+++
@@ -1,9 +0,0 @@
-const DB_URL = process.env.DB_URL || '';
-const MAIL_USER = process.env.MAIL_USER || '';
-const MAIL_PASS = process.env.MAIL_PASS || '';
-
-module.exports = {
-  DB_URL,
-  MAIL_USER,
-  MAIL_PASS,
-};
